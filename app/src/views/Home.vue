020042073c3d569c88674d8f05003d5461a5c85c
David Dacruz <dvddcrz@gmail.com>
2020-01-26 11:34:34 +0100

    working on static layout for the front

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,6 +1,35 @@
 <template>
-  <div class="bg-red-600 h-screen">
-    
+  <div>
+    <div class="mb-12">
+      <img class="rounded" src="@/assets/img/slide1.jpg" alt="slider 1">
+    </div>
+    <div class="categories flex items-center mb-12 -mx-4">
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg p-4 border border-gray-700 shadow-custom hover:bg-gray-900">
+          <img src="@/assets/img/disney_logo.svg" alt="">
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg p-4 border border-gray-700 shadow-custom hover:bg-gray-900">
+          <img src="@/assets/img/disney_logo.svg" alt="">
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg p-4 border border-gray-700 shadow-custom hover:bg-gray-900">
+          <img src="@/assets/img/disney_logo.svg" alt="">
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg p-4 border border-gray-700 shadow-custom hover:bg-gray-900">
+          <img src="@/assets/img/disney_logo.svg" alt="">
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg p-4 border border-gray-700 shadow-custom hover:bg-gray-900">
+          <img src="@/assets/img/disney_logo.svg" alt="">
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
