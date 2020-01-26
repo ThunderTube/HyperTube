8cd45e88fc3db2318fe356f4531f94350d14c3d6
David Dacruz <dvddcrz@gmail.com>
2020-01-26 15:03:33 +0100

    home page and nav layout

---
+++ app/src/components/MovieThumbnail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="shadow-xl m-2">
+    <a href="#">
+      <img class="movie-thumbnail" src="@/assets/img/star_wars.jpg" alt />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "MovieThumbnail"
+}
+</script>
+
+
+<style lang="css" scoped>
+.movie-thumbnail {
+  opacity: 0.75;
+  transition: all 0.3s;
+}
+
+.movie-thumbnail:hover {
+  opacity: 1;
+  transform: scale(1.1);
+}
+</style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,42 +1,30 @@
 <template>
-  <div>
-    <div class="mb-12">
-      <img class="rounded" src="@/assets/img/slide1.jpg" alt="slider 1">
-    </div>
-    <div class="categories flex items-center mb-12 -mx-4">
-      <div class="w-1/5 px-4">
-        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg p-4 border border-gray-700 shadow-custom hover:bg-gray-900">
-          <img src="@/assets/img/disney_logo.svg" alt="">
-        </a>
-      </div>
-      <div class="w-1/5 px-4">
-        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg p-4 border border-gray-700 shadow-custom hover:bg-gray-900">
-          <img src="@/assets/img/disney_logo.svg" alt="">
-        </a>
-      </div>
-      <div class="w-1/5 px-4">
-        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg p-4 border border-gray-700 shadow-custom hover:bg-gray-900">
-          <img src="@/assets/img/disney_logo.svg" alt="">
-        </a>
-      </div>
-      <div class="w-1/5 px-4">
-        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg p-4 border border-gray-700 shadow-custom hover:bg-gray-900">
-          <img src="@/assets/img/disney_logo.svg" alt="">
-        </a>
-      </div>
-      <div class="w-1/5 px-4">
-        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg p-4 border border-gray-700 shadow-custom hover:bg-gray-900">
-          <img src="@/assets/img/disney_logo.svg" alt="">
-        </a>
+  <div class="w-full container overflow-hidden mx-auto min-h-screen">
+    <div class="px-2">
+      <div class="flex flex-wrap -mx-2">
+        <div v-for="i in number_of_movies" :key="i" class="w-full md:w-1/3 lg:w-1/5 p-2">
+          <movie-thumbnail />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import MovieThumbnail from "@/components/MovieThumbnail.vue";
 
 export default {
-  name: 'home',
-
-}
+  name: "home",
+  components: {
+    MovieThumbnail
+  },
+  data() {
+    return {
+      number_of_movies: 50
+    }
+  }
+};
 </script>
+
+<style lang="css" scoped>
+</style>
