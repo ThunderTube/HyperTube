a8b03eb552932db840714db1e2cf7f48a125235e
Iscia AMBAYRAC <iambayra@e1r6p19.42.fr>
2020-02-05 18:50:11 +0100

    Started user part

--- api/index.js
+++ api/index.js
@@ -1,20 +1,34 @@
 const connectDB = require('./config/db');
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
 
 // Connect to database
 connectDB();
 
-const app = express()
+// Route files
+const auth = require('./routes/auth');
+
+const app = express();
+
+// Body parser
+app.use(express.json());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.listen(process.env.PORT, (err) => {
+app.use('/api/v1/auth', auth);
+
+app.listen(process.env.PORT, err => {
   if (err) {
-    return console.log('something bad happened', err)
+    return console.log('something bad happened', err);
   }
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`)
-})
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+  );
+});
