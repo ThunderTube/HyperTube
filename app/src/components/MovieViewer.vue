402b2cd04e76887ed62e6571b570d3cda1c98ee1
Baptiste DEVESSIER <bdevessi@student.42.fr>
2020-02-28 17:50:29 +0100

    feat(movie): prepare the integration of a loader and data fetching

---
+++ app/src/components/MovieViewer.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="w-full container pt-20 mx-auto">
+    <h2 class="text-white font-semibold text-3xl tracking-wider mb-6">
+      {{ title }} ({{ year }})
+    </h2>
+
+    <video
+      preload="none"
+      poster="http://image.tmdb.org/t/p/w500/AtsgWhDnHTq68L0lLsUrCnM7TjG.jpg"
+      class="w-full object-cover object-top player"
+    ></video>
+
+    <div class="py-4 text-white">
+      <p class="text-xl mb-2" :title="`The movie lasts ${formattedRuntime}`">
+        ⏱ {{ formattedRuntime }}
+      </p>
+
+      <blockquote class="text-xl tracking-wide mb-3">
+        {{ description }}
+      </blockquote>
+
+      <tag v-for="genre in genres" :key="genre" big>
+        {{ genre }}
+      </tag>
+    </div>
+  </div>
+</template>
+
+<script>
+import Tag from '@/components/Tag.vue'
+
+export default {
+  name: 'MovieViewer',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    year: {
+      type: Number,
+      required: true
+    },
+    runtime: {
+      type: Number,
+      required: true
+    },
+    genres: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    formattedRuntime() {
+      const hours = this.runtime / 60
+      const minutes = this.runtime % 60
+
+      return `${hours.toFixed(0)}h${minutes.toFixed(0)}`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.player {
+  height: 500px;
+}
+</style>
