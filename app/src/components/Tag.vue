dcdb489ae1bcc96d113ef2599acd79cc3d75b296
Baptiste DEVESSIER <bdevessi@student.42.fr>
2020-02-28 10:23:36 +0100

    feat(search): show movies genres using tag components

---
+++ app/src/components/Tag.vue
@@ -0,0 +1,23 @@
+<template functional>
+  <span
+    class="inline-block rounded-full px-2 py-1 mb-1 mr-1 bg-gray-700 text-gray-200 text-sm font-semibold tag"
+  >
+    <slot />
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'Tag'
+}
+</script>
+
+<script lang="scss" scoped>
+.tag {
+  &::before {
+    @apply mr-1;
+
+    content: '#';
+  }
+}
+</script>
