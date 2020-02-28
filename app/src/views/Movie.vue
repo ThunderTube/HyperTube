673c45b3dcf0c02256931cb125ce760612a07ea3
Baptiste DEVESSIER <bdevessi@student.42.fr>
2020-02-28 16:02:19 +0100

    feat(movie): start to redesign the movie page

--- app/src/views/Movie.vue
+++ app/src/views/Movie.vue
@@ -1,56 +1,61 @@
 <template>
-  <div>
-    <div
-      class="min-h-screen bg-cover"
-      :style="{ backgroundImage: `url('${background}')` }"
-    >
-      <div class="pt-32 pl-8 mb-2"></div>
+  <div class="w-full container pt-20 mx-auto">
+    <h2 class="text-white font-semibold text-3xl tracking-wider mb-6">
+      {{ title }} ({{ year }})
+    </h2>
 
-      <div class="pt-32 pl-24 mb-4">
-        <button
-          class="px-6 py-2 bg-white text-black font-bold uppercase rounded flex items-center hover:bg-gray-300"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <path d="M8 5v14l11-7z" />
-            <path d="M0 0h24v24H0z" fill="none" />
-          </svg>
-          <span class="pl-1">{{ $t('movie.play') }}</span>
-        </button>
-      </div>
+    <video
+      preload="none"
+      poster="http://image.tmdb.org/t/p/w500/AtsgWhDnHTq68L0lLsUrCnM7TjG.jpg"
+      class="w-full object-cover object-top player"
+    ></video>
 
-      <div class="pl-24 text-gray-500 mb-8">
-        PG-13 &middot; 2019 &middot; 2h 4m &middot; Action, Adventure, Science
-        Fiction
-      </div>
+    <div class="py-4 text-white">
+      <p class="text-xl mb-2" :title="`The movie lasts ${formattedRuntime}`">
+        ⏱ {{ formattedRuntime }}
+      </p>
 
-      <div class="w-96 xl:w-1/3 pl-24 leading-loose">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus fugit
-        tenetur velit quasi soluta sunt suscipit, eveniet dolorum. Tenetur
-        architecto voluptatum quis, vitae autem, vero tempora libero impedit
-        culpa debitis, quisquam nam nemo quidem modi sunt illo consequuntur
-        maiores magni.
-      </div>
-      <!-- <img src="@/assets/captain_marvel_background.jpg" alt="captain marvel"> -->
-    </div>
-    <div
-      class="bg-red-600 h-screen flex items-center justify-center uppercase text-white text-5xl"
-    >
-      <h1>Comments</h1>
+      <blockquote class="text-xl tracking-wide mb-3">
+        {{ description }}
+      </blockquote>
+
+      <tag v-for="genre in genres" :key="genre" big>
+        {{ genre }}
+      </tag>
     </div>
   </div>
 </template>
 
 <script>
+import Tag from '@/components/Tag.vue'
+
 export default {
+  components: {
+    Tag
+  },
   data() {
     return {
-      background: require('@/assets/img/captain_marvel_background.jpg')
+      title: "Shrek 3 - l'Ogre",
+      year: '2018',
+      runtime: 104,
+      genres: ['action', 'war', 'science-fiction'],
+      description:
+        'Elsa, Anna, Kristoff and Olaf head far into the forest to learn the truth about an ancient mystery of their kingdom.'
+    }
+  },
+  computed: {
+    formattedRuntime() {
+      const hours = this.runtime / 60
+      const minutes = this.runtime % 60
+
+      return `${hours.toFixed(0)}h${minutes.toFixed(0)}`
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.player {
+  height: 500px;
+}
+</style>
