237321284b8db16444d9c2c4e1f8e6f3f84a310f
Baptiste DEVESSIER <bdevessi@student.42.fr>
2020-03-11 14:00:22 +0100

    feat(update-details): let the user select its favorite language

--- app/src/views/Me.vue
+++ app/src/views/Me.vue
@@ -64,6 +64,38 @@
                 @change="handleFileUpload()"
               />
             </div>
+
+            <div>
+              <label class="">
+                <p class="capitalize font-bold mt-2">
+                  {{ $t('profile.favorite-language') }}
+                </p>
+
+                <div class="relative w-full">
+                  <select
+                    v-model="details.form.favoriteLanguage"
+                    class="block appearance-none w-full border bg-white border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:border-gray-500"
+                  >
+                    <option value="en">EN</option>
+                    <option value="fr">FR</option>
+                  </select>
+
+                  <div
+                    class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+                  >
+                    <svg
+                      class="fill-current h-4 w-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </label>
+            </div>
           </div>
           <div v-else-if="password.visible">
             <app-input
@@ -109,7 +141,8 @@ export default {
           lastName: '',
           firstName: '',
           password: '',
-          profilePicture: null
+          profilePicture: null,
+          favoriteLanguage: 'en'
         }
       },
       password: {
@@ -224,6 +257,7 @@ export default {
     this.details.form.username = this.getAuthData.username
     this.details.form.firstName = this.getAuthData.firstName
     this.details.form.lastName = this.getAuthData.lastName
+    this.details.form.favoriteLanguage = this.getAuthData.favoriteLanguage
   }
 }
 </script>
