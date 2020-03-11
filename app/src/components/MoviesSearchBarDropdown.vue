8dd922e2e56bf7b41143c03296d5dd66000614c3
Baptiste DEVESSIER <bdevessi@student.42.fr>
2020-03-11 13:54:12 +0100

    improvement(search): add arrow to search dropdown component

--- app/src/components/MoviesSearchBarDropdown.vue
+++ app/src/components/MoviesSearchBarDropdown.vue
@@ -4,16 +4,31 @@
       {{ label }}
     </p>
 
-    <select
-      :value="value"
-      @change="$emit('input', $event.target.value)"
-      :name="name"
-      class="block appearance-none border bg-gray-800 text-gray-400 rounded px-2 py-2 cursor-pointer w-full transition-colors duration-150 focus:outline-none border-transparent focus:border-gray-200"
-    >
-      <option v-for="{ text, value } in options" :key="value" :value="value">
-        {{ text }}
-      </option>
-    </select>
+    <div class="relative w-full">
+      <select
+        :value="value"
+        @change="$emit('input', $event.target.value)"
+        :name="name"
+        class="block appearance-none border bg-gray-800 text-gray-400 rounded px-2 py-2 cursor-pointer w-full transition-colors duration-150 focus:outline-none border-transparent focus:border-gray-200"
+      >
+        <option v-for="{ text, value } in options" :key="value" :value="value">
+          {{ text }}
+        </option>
+      </select>
+      <div
+        class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-600"
+      >
+        <svg
+          class="fill-current h-5 w-5"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <path
+            d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+          />
+        </svg>
+      </div>
+    </div>
   </label>
 </template>
 
