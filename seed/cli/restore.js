90b7380ff0de3eba0d824cc6eb973abd0176167a
Devessier <baptiste@devessier.fr>
2020-03-19 13:01:56 +0100

    improvement: enable torrents removing on restoring
    By setting `HIDE` environment variable to something `truthy`
    `torrents` property is skiped for all movies.

--- seed/cli/restore.js
+++ seed/cli/restore.js
@@ -6,13 +6,25 @@ const { Movie } = require('../../api/src/models/Movie');
 const connectDB = require('../../api/src/config/db');
 const { DUMP_FILE } = require('./constants');
 
+function removeTorrents({ torrents, ...props }) {
+    return {
+        ...props,
+        torrents: [],
+    };
+}
+
 async function cli() {
     try {
+        const HIDE_SENSIBLE_DATA = Boolean(process.env.HIDE);
+
         await connectDB();
 
         console.log('> Start to get all movies from the dump file ...');
 
-        const movies = await readFile(DUMP_FILE);
+        let movies = JSON.parse(await readFile(DUMP_FILE));
+        if (HIDE_SENSIBLE_DATA) {
+            movies = movies.map(removeTorrents);
+        }
 
         console.log('> Received all the movies from the dump file');
 
@@ -21,7 +33,7 @@ async function cli() {
 
         console.log('> Start insert all files into the DB ...');
 
-        await Movie.collection.insertMany(JSON.parse(movies));
+        await Movie.collection.insertMany(movies);
 
         console.log(`> Restored the \`movies\` collection from ${DUMP_FILE}`);
 
